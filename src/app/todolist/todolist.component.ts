bbd72440e078d4681fcda8608b5dc10f1f5f67a7
anoriar <borisov.alex12@gmail.com>
2017-12-03 13:32:24 +0330

    todolist

---
+++ src/app/todolist/todolist.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent {
+
+	newTodoTitle:string='';
+
+	todos:Array<Todo> =[
+	];
+
+	addTodo() {
+		if(!this.newTodoTitle){
+			return;	
+		}
+		const newTodo: Todo ={
+			title:this.newTodoTitle,
+			complete:false
+		};
+		this.todos.push(newTodo);
+
+		this.newTodoTitle='';
+	}
+
+	deleteTodo(index){
+		this.todos.splice(index,1);
+	}
+}
+
+interface Todo{
+	title:string;
+	complete:boolean;
+}
